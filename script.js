338e2617b65582eb5c96166b1fbee41c357535b2
Hannah-Bleck <127896891+Hannah-Bleck@users.noreply.github.com>
2023-11-08 19:07:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+      window.onscroll = function() {
+        myFunction()
+    };
+      
+      function myFunction() {
+        if (document.documentElement.scrollTop > 350) {
+            document.getElementById("projectsdiv").className = "slideUp";
+            document.getElementById("projectsdiv").style.visibility = "visible";
+            document.getElementById("secondseperator").style.visibility = "visible";
+        }else if(document.documentElement.scrollTop < 400 && document.documentElement.scrollTop > 200){
+            document.getElementById("projectsdiv").className = "slideDown";
+        }else{
+            document.getElementById("projectsdiv").style.visibility = "collapse";
+            document.getElementById("secondseperator").style.visibility = "collapse";
+        }
+      }
+
+      document.getElementsByClassName("thumb").onhover = function() {
+        hoverProjects()
+    };
+
+    function hoverProjects(){
+        document.getElementsById("thumb1").style.backgroundColor = "red";
+    }
